5d0ee77a572d7862de710d0df7db19cae6f8e793
Si Lam <silam@hotmail.com>
2019-02-03 11:35:00 -0600

    confusion express put I

--- app.js
+++ app.js
@@ -6,6 +6,22 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var dishRouter = require('./routes/dishRouter');
+var promoRouter = require('./routes/promoRouter');
+var leaderRouter = require('./routes/leaderRouter');
+
+
+const mongoose = require('mongoose');
+
+const Dishes = require('./models/dishes');
+
+const url = 'mongodb://localhost:27017/conFusion';
+const connect = mongoose.connect(url);
+
+connect.then((db) => {
+    console.log("Connected correctly to server");
+}, (err) => { console.log(err); });
+
 
 var app = express();
 
@@ -21,6 +37,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter);
+app.use('/dishes', dishRouter);
+
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
